9ddea7bc50eb683474c3f5806ad3d6fcca60cca6
irvingramirez1710 <irvingcanond@hotmail.com>
2021-08-07 22:39:00 -0500

    se agrega el proyecto

---
+++ indiabana/src/Components/Login/login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './login.scss'
+
+export default function login() {
+    return (
+        <div>
+                <h1>Login</h1>
+        </div>
+    )
+}
+
---
+++ indiabana/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Login from './Components/Login/login';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Login />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
